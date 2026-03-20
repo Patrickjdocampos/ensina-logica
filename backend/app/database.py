29ae5e1b41f0e225288cb64679fb73404c34d4ae
Patrickjdocampos <patrickjdocampos@gmail.com>
2026-03-20 10:53:04 -0300

    fix: adicionando pool_pre_ping na conexao com postgres

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,16 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Pega a URL do arquivo .env. Se não achar, usa o SQLite como fallback de segurança.
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./ensina_logica.db")
 
-# O SQLite exige 'check_same_thread', mas o Postgres não.
 if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
     )
 else:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        pool_pre_ping=True # <-- Adicionado aqui
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
